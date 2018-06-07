43e9d9f4d5d1ea9f4009684741253dcb2f1f2af6
ekevjn <amazon.n8@gmail.com>
2018-06-07 15:01:01 +0700

    somehting newd

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.get('/', function (req, res) {
 // for Facebook verification
 app.get('/webhook', function (req, res) {
 	let messaging_events = req.body.entry[0].messaging;
+	console.log(req.body.entry);
 	for (let i = 0; i < messaging_events.length; i++) {
 		let event = req.body.entry[0].messaging[i];
 		let sender = event.sender.id;
