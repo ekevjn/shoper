862d7d2d5e216dbb2f25651f10d8dd9e49ac5b74
unknown <trungvd@trungvd-w7.vsii.com>
2018-06-07 09:43:44 +0700

    add new mm

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
+const PAGE_ACCESS_TOKEN = process.env.VERIFY_TOKEN;
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -32,6 +33,46 @@ app.get('/webhook/', function (req, res) {
     res.sendStatus(200)
 })
 
+app.post('/webhook', (req, res) => {  
+
+	// Parse the request body from the POST
+	let body = req.body;
+  
+	// Check the webhook event is from a Page subscription
+	if (body.object === 'page') {
+  
+	  // Iterate over each entry - there may be multiple if batched
+	  body.entry.forEach(function(entry) {
+
+		// Gets the body of the webhook event
+		let webhook_event = entry.messaging[0];
+		console.log(webhook_event);
+	  
+	  
+		// Get the sender PSID
+		let sender_psid = webhook_event.sender.id;
+		console.log('Sender PSID: ' + sender_psid);
+	  
+		// Check if the event is a message or postback and
+		// pass the event to the appropriate handler function
+		if (webhook_event.message) {
+		  handleMessage(sender_psid, webhook_event.message);        
+		} else if (webhook_event.postback) {
+		  handlePostback(sender_psid, webhook_event.postback);
+		}
+		
+	  });
+  
+	  // Return a '200 OK' response to all events
+	  res.status(200).send('EVENT_RECEIVED');
+  
+	} else {
+	  // Return a '404 Not Found' if event is not from a page subscription
+	  res.sendStatus(404);
+	}
+  
+  });
+
 function sendTextMessage(sender, text) {
     let messageData = { text:text }
     request({
@@ -50,6 +91,48 @@ function sendTextMessage(sender, text) {
 	    }
     })
 }
+
+function handleMessage(sender_psid, received_message) {
+
+	let response;
+  
+	// Check if the message contains text
+	if (received_message.text) {    
+  
+	  // Create the payload for a basic text message
+	  response = {
+		"text": `You sent the message: "${received_message.text}". Now send me an image!`
+	  }
+	}  
+	
+	// Sends the response message
+	callSendAPI(sender_psid, response);    
+  }
+
+  function callSendAPI(sender_psid, response) {
+	// Construct the message body
+	let request_body = {
+	  "recipient": {
+		"id": sender_psid
+	  },
+	  "message": response
+	}
+  
+	// Send the HTTP request to the Messenger Platform
+	request({
+	  "uri": "https://graph.facebook.com/v2.6/me/messages",
+	  "qs": { "access_token": PAGE_ACCESS_TOKEN },
+	  "method": "POST",
+	  "json": request_body
+	}, (err, res, body) => {
+	  if (!err) {
+		console.log('message sent!')
+	  } else {
+		console.error("Unable to send message:" + err);
+	  }
+	}); 
+  }
+  
 
 // Spin up the server
 app.listen(app.get('port'), function() {
