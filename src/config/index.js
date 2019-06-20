416af957b4b2bbf6fd810799177f0d7c3950ec1d
Vu Dang Trung <vu.dang.trung@vsi-international.com>
2019-06-20 15:46:47 +0700

    change api version

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,6 @@
 module.exports = {
-  PAGE_ACCESS_TOKEN: process.env.PAGE_ACCESS_TOKEN ? process.env.PAGE_ACCESS_TOKEN: 'EAACrt0LSQ20BALasuHTHs1GZAv5ZAePr5rE89HnQcxvMq3CzZAymhMuzBvA2n10hcBywkUOiSiboPudpPM8QqZAssVqqWu3G2GldEzKp0qYJfGfO3NgX8xlbuvzbbqZAIuLRNZAJSKhctWCV8xAsN4Lvlt6mQlSmqczZBZAd2TQZA54MhIXY1v6Cj',
+  PAGE_ACCESS_TOKEN: process.env.PAGE_ACCESS_TOKEN
+    ? process.env.PAGE_ACCESS_TOKEN
+    : 'EAACrt0LSQ20BAPsPLD8vnYh3ZAx0qt2nlZAGtENVNCvolOvQX2o4lIZAyP4sS2j74W5ZCaYaFi5Nrvs5pAIngg2mr2tUHnaHqrg79uL3pl3j2dO34CBISvkHijRrHfnzsBPXHLrauQqPeOQNt6ReqeCMc0k70iZBlZCbV6T5zP9wZDZD',
   secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret'
 };
